be41a0267b5af09d329f218034119479e635aea6
gangyi89@gmail.com <gangyi89@gmail.com>
2020-12-23 01:53:30 +0800

    update token

--- src/agora.config.js
+++ src/agora.config.js
@@ -1,3 +1,3 @@
 export const AGORA_APP_ID = "cb401c85ffe84530b67ae13c8488bb52"; // set your app id here
 export const AGORA_TOKEN =
-  "006cb401c85ffe84530b67ae13c8488bb52IAA0Hjy8RohTnO1zGt/U65Orfk+a3SKJl4HUbRPUixSDEAx+f9gAAAAAEAD26wA6tevhXwEAAQC16+Ff";
+  "006cb401c85ffe84530b67ae13c8488bb52IABqncB+5/N7iQ0x+DldteRVECt0oXB8zepaxE5BrUxJ8Qx+f9gAAAAAEAD26wA6p3LjXwEAAQCncuNf";
